fixed minus signs

warp.py
@@ -150,23 +150,22 @@ def interpol_drift(drift, params):
 def calc_entr(im, params):
     '''calculate image entropy using fast histogram method.'''
     bins = np.linspace(*params['entropybins'])
-    maxS = np.log(params['entropybins'][-1])
-    #bins = np.linspace()
-    #print np.max(im)
-    #bins = 64
+    nbin = 1.0*params['entropybins'][-1]
+    maxS = np.log(nbin)
+   
     hist, _ = np.histogram(im, bins)
     # shift to avoid log 0
     hist = hist + 1
-    area = 1.0*np.sum(hist)*np.diff(bins)[0]
+    area = 1.0*np.sum(hist)
     entr = hist/area
     # first entropy calculation with larger part of histogram
-    entr =  -entr*np.log(entr)/maxS#*hist, 
+    entr =  -entr*np.log(entr)
      # take only tail
     cut = 10
     entr2 = hist[cut:]/1.0/np.sum(hist[cut:])
-    entr2 = -entr2*np.log(entr2)/np.log(params['entropybins'][-1]-cut)#*hist[cut:]
+    entr2 = -entr2*np.log(entr2)#*hist[cut:]
     
-    return entr, entr2
+    return np.sum(entr)/maxS, np.sum(entr2)/np.log(nbin-cut)
 
 def difference_entropy(im1,im2, cms, params):
     """Calculates the entropy of the difference image in the region of intrest."""
@@ -182,7 +181,7 @@ def difference_entropy(im1,im2, cms, params):
     #rest of image entropy    
     #entr1, area1 = calc_entr(np.abs(diff))
 
-    return np.sum(entr2), np.sum(entr1)
+    return entr2, entr1
 
 def pumping(params, roi):
     """finds periodic motion from images using entropy based difference detection